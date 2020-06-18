5d9463c7ffc99b4095dffa144bc247dd67f247d6
minizbot2012 <mini@zbots.org>
2020-06-17 20:04:30 -0400

    Modernized, updated, and patch counters

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 /* global ngapp, xelib */
 const sigs = ["TREE", "FLOR"];
 let ensureVMAD = function(record, version = 5, format = 2) {
-    let vmad = xelib.AddElement(record, 'VMAD');
-    if (!vmad) throw new Error(`Failed to create VMAD element on record: ${xelib.LongName(record)}`);
-    xelib.SetValue(vmad, 'Version', version.toString());
-    xelib.SetValue(vmad, 'Object Format', format.toString());
+  let vmad = xelib.AddElement(record, 'VMAD');
+  if (!vmad) throw new Error(`Failed to create VMAD element on record: ${xelib.LongName(record)}`);
+  xelib.SetValue(vmad, 'Version', version.toString());
+  xelib.SetValue(vmad, 'Object Format', format.toString());
 };
 let procBlock = function(sig) {
   return {
-    load: () => {
-      return {
-        signature: sig
-      }
+    load: {
+      signature: sig
     },
     patch: function (record, helpers, settings, locals) {
       ensureVMAD(record)
       if(!xelib.HasScript(record, "FloraFix")) {
         xelib.AddScript(record, "FloraFix");
-        helpers.logMessage("Patched: "+sig + " : "+ xelib.FullName(record));
       }
+      locals.count++;
     }
   }
 };
 registerPatcher({
-    info: info,
-    gameModes: [xelib.gmTES5, xelib.gmSSE],
-    settings: {
-      label: 'Respawn patcher',
-      hide: true
-    },
-    execute: {
-      process: sigs.map(procBlock)
+  info: info,
+  gameModes: [xelib.gmTES5, xelib.gmSSE],
+  settings: {
+    label: 'Respawn patcher',
+    hide: true
+  },
+  execute: function(patch, helpers, settings, locals) {
+    return {
+      initialize: function(patch, helpers, settings, locals) {
+        locals.count = 0;
+      },
+      process: sigs.map(procBlock),
+      finalize: function(patch, helpers, settings, locals) {
+        helpers.logMessage("Patched "+ locals.count + " TREE/FLOR Records")
+      }
     }
+  }
 });
